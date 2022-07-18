50f3701bebabb42ce6727a8af09f0b6c435c9c9c
Will Baker <willcbaker@users.noreply.github.com>
2022-07-18 10:32:43 -0700

    Update farmworld.py

--- farmworld.py
+++ farmworld.py
@@ -9,44 +9,8 @@ import math  # used for some basic trigonometry
 import PySimpleGUI as sg  # used for the Gui
 import CaladanAPI  # example API library included with this example
 import farmworld_config  # simple convinience, used to store
+import gui_config # TODO: simplify example to use gui config
 
-url = "https://beta-caladan.polymathrobotics.dev/api/"
-api = CaladanAPI.simple_api("", "", "")
-scale = 400000.0  # Used to change scale of drawing scene
-font = ("Courier New", 7)
-
-sg.theme("DarkGrey10")
-layout = [
-    [sg.Text("Please provide Token and Key below", key="-PROMPT-")],
-    [sg.Input("Bearer Token")],
-    [sg.Input("Unique Key")],
-    [sg.Button("Connect"), sg.Text("", key="-VEL-"), sg.Text("", key="-POSE-")],
-    [
-        sg.Graph(
-            canvas_size=(0.001248654 * scale, 0.001578659 * scale),
-            graph_bottom_left=(0, 0),
-            graph_top_right=(0.001248654 * scale, 0.001578659 * scale),
-            background_color="#1C1E23",
-            enable_events=True,
-            key="graph",
-        )
-    ],
-    [
-        sg.Button("Start Tilling"),
-        sg.Button("STOP", button_color=("white", "red")),
-        sg.Text("", key="-STATUS-", size=(45, None)),
-    ],
-    [sg.Button("Return to Equipment Shed")],
-]
-
-
-window = sg.Window(
-    "Polymath Robotics Caladan Farmworld Example",
-    layout,
-    icon="icon.png",
-    finalize=True,
-)
-graph = window["graph"]
 
 # GPS Coordinates hardcoded for example simplicity
 # bottom left: 37.72475936903438, -120.99873450248205
@@ -56,6 +20,10 @@ graph = window["graph"]
 # Hence, window will be 0.001248654 by 0.001578659 in lat/lon coordinates
 # Multiplying by 400000 to get pixel space (so ~ 500 x 632)
 # In a real application, please convert correctly, this only works well for small toy examples
+
+
+GUI_SCALE = 400000.0 # Used to change scale of drawing scene -> lat/lon
+URL_API = "https://beta-caladan.polymathrobotics.dev/api/"
 
 
 def latlon_to_pixelXY(lat, lon):
@@ -114,70 +82,77 @@ def graph_clean():
     )
 
 
-while True:  # Main UI Loop
-    event, values = window.read()
+def main():
+    api = CaladanAPI.simple_api("", "", "")
+    window = create_window(GUI_SCALE)
+    
+    while True:  # Main UI Loop
+        event, values = window.read()
 
-    if event == sg.WIN_CLOSED:  # Handle closing window
-        break
+        if event == sg.WIN_CLOSED:  # Handle closing window
+            break
 
-    if (
-        window["Connect"].get_text() == "Connected"
-    ):  # Only update and accept other commands if already connected
-        window["-VEL-"].update(
-            str(round(farmworld_config.odom["odometry"]["linear.x"], 2)) + " m/s  "
-        )
-        current_pose = (
-            "lat: "
-            + str(round(farmworld_config.odom["position"]["latitude"], 6))
-            + " lon: "
-            + str(round(farmworld_config.odom["position"]["longitude"], 6))
-            + " angle: "
-            + str(round(farmworld_config.odom["orientation"], 3))
-        )
-        window["-POSE-"].update(current_pose)
-        window["-STATUS-"].update(farmworld_config.stat)
-        draw_pose = latlon_to_pixelXY(
-            farmworld_config.odom["position"]["latitude"],
-            farmworld_config.odom["position"]["longitude"],
-        )
-        position = graph.draw_circle(
-            draw_pose, 3, fill_color="#1C1E23", line_color="white"
-        )
-
-        if event == "Return to Equipment Shed":
-            graph_clean()
-            send_goal(37.72488196909861, -120.99962795857607, 0)
-
-        if event == "Start Tilling":
-            graph_clean()
-            window.perform_long_operation(tilling, "-tilling DONE-")
-
-        if event == "STOP":
-            graph_clean()
-            api.cancel_prev_goal()
-
-        if event == "graph":
-            graph_clean()
-            x, y = values["graph"]
-            lat, lon = pixelXY_to_latlon(x, y)
-            send_goal(
-                lat, lon, yaw=0
-            )  # NOTE: Clicking on the map always sends orientation 0!
-
-    if event == "Connect":  # First time clicking the connect button, check we get UUID
-        api = CaladanAPI.simple_api(url, values[1], values[0])
-        uuid_response = api.get_uuid()
-        if "uuid" in uuid_response:
-            window["-PROMPT-"].update(
-                "Successfully connected to vehicle " + uuid_response["uuid"]
+        if (
+            window["Connect"].get_text() == "Connected"
+        ):  # Only update and accept other commands if already connected
+            window["-VEL-"].update(
+                str(round(farmworld_config.odom["odometry"]["linear.x"], 2)) + " m/s  "
             )
-            window["Connect"].update(button_color=("black", "green"))
-            window.perform_long_operation(update_loop, "-OPERATION DONE-")
-            window["Connect"].update("Connected")
-            graph_clean()
-        else:
-            window["-PROMPT-"].update(
-                uuid_response
-            )  # otherwise, print returned error message
+            current_pose = (
+                "lat: "
+                + str(round(farmworld_config.odom["position"]["latitude"], 6))
+                + " lon: "
+                + str(round(farmworld_config.odom["position"]["longitude"], 6))
+                + " angle: "
+                + str(round(farmworld_config.odom["orientation"], 3))
+            )
+            window["-POSE-"].update(current_pose)
+            window["-STATUS-"].update(farmworld_config.stat)
+            draw_pose = latlon_to_pixelXY(
+                farmworld_config.odom["position"]["latitude"],
+                farmworld_config.odom["position"]["longitude"],
+            )
+            position = graph.draw_circle(
+                draw_pose, 3, fill_color="#1C1E23", line_color="white"
+            )
 
-window.close()
+            if event == "Return to Equipment Shed":
+                graph_clean()
+                send_goal(37.72488196909861, -120.99962795857607, 0)
+
+            if event == "Start Tilling":
+                graph_clean()
+                window.perform_long_operation(tilling, "-tilling DONE-")
+
+            if event == "STOP":
+                graph_clean()
+                api.cancel_prev_goal()
+
+            if event == "graph":
+                graph_clean()
+                x, y = values["graph"]
+                lat, lon = pixelXY_to_latlon(x, y)
+                send_goal(
+                    lat, lon, yaw=0
+                )  # NOTE: Clicking on the map always sends orientation 0!
+
+        if event == "Connect":  # First time clicking the connect button, check we get UUID
+            api = CaladanAPI.simple_api(url, values[1], values[0])
+            uuid_response = api.get_uuid()
+            if "uuid" in uuid_response:
+                window["-PROMPT-"].update(
+                    "Successfully connected to vehicle " + uuid_response["uuid"]
+                )
+                window["Connect"].update(button_color=("black", "green"))
+                window.perform_long_operation(update_loop, "-OPERATION DONE-")
+                window["Connect"].update("Connected")
+                graph_clean()
+            else:
+                window["-PROMPT-"].update(
+                    uuid_response
+                )  # otherwise, print returned error message
+
+    window.close()
+
+if __name__ == "__main__":
+    main()
